931165c002824de412fde31bd1cdf1442cb6a3fe
LuisMargalhaes <126782198+LuisMargalhaes@users.noreply.github.com>
2024-04-30 10:39:21 -0300

    Add files via upload

---
+++ lib/pages/LoginPage.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Column(
+            children: [
+              Padding(padding: EdgeInsets.symmetric(vertical: 20),
+              child: Image.asset("assets/login.png"),
+      
+            
+          ),
+          Container()
+            ]
+          ),
+        ),
+      ),
+      ),
+    );
+  }
+}
